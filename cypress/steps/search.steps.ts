3cae2347cbf84e383aabfab5a7d6d7d5cc448a0e
unknown <hammami.akrem03@gmail.com>
2025-12-04 09:15:09 +0100

    style: Remove remaining unused variables
    - Remove unused 'Given' and 'keyword' in search.steps.ts
    - Remove unused 'req' in demowebshop.interceptor.ts

--- cypress/steps/search.steps.ts
+++ cypress/steps/search.steps.ts
@@ -1,5 +1,5 @@
 // cypress/steps/search.steps.ts
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 When("I type {string} in the search box", (searchTerm: string) => {
   cy.get("#small-searchterms").type(searchTerm);
@@ -10,7 +10,7 @@ Then("I should see search results", () => {
   cy.get(".product-item").should("have.length.greaterThan", 0);
 });
 
-Then("the results should contain {string}", (keyword: string) => {
+Then("the results should contain {string}", () => {
   cy.get(".product-title").should("exist");
 });
 
--- cypress/support/interceptors/demowebshop.interceptor.ts
+++ cypress/support/interceptors/demowebshop.interceptor.ts
@@ -2,17 +2,17 @@
 // Useful intercepts for DemoWebShop
 export const intercepts = {
   loginSuccess: () => {
-    cy.intercept('POST', '/login', (req) => {
+    cy.intercept("POST", "/login", () => {
       // let real request go through, or mock response if desired
-    }).as('loginRequest')
+    }).as("loginRequest");
   },
 
   addToCart: () => {
-    cy.intercept('POST', '/addproducttocart/*', (req) => {
+    cy.intercept("POST", "/addproducttocart/*", (req) => {
       // optionally stub success response
       req.reply((res) => {
-        res.send({ statusCode: 200, body: { success: true } })
-      })
-    }).as('addToCart')
-  }
-}
+        res.send({ statusCode: 200, body: { success: true } });
+      });
+    }).as("addToCart");
+  },
+};
